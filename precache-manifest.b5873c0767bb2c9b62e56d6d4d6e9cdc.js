f62f4e3f5d6c8c3dabd9dd6d36afe7f9ae79943a
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-06-15 18:15:16 +0800

    Updates

---
+++ precache-manifest.b5873c0767bb2c9b62e56d6d4d6e9cdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52d8277442f5961e3cd37bcf25caba7",
+    "url": "/kontakmanager/index.html"
+  },
+  {
+    "revision": "4fd0cb7c7055d43a546b",
+    "url": "/kontakmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f2d92b03e3bc34465d8a",
+    "url": "/kontakmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "4fd0cb7c7055d43a546b",
+    "url": "/kontakmanager/static/js/2.996541e6.chunk.js"
+  },
+  {
+    "revision": "f2d92b03e3bc34465d8a",
+    "url": "/kontakmanager/static/js/main.c9ba8f35.chunk.js"
+  },
+  {
+    "revision": "3fecca47443d82be8929",
+    "url": "/kontakmanager/static/js/runtime~main.2cbb8ade.js"
+  }
+]);
